9569764d84695992d0588dcca97d56a3e8464d0b
Daria Astapenko <miss.daria.astapenko@gmail.com>
2024-11-15 21:18:43 +0100

    updating node and npm version

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { Amplify } from 'aws-amplify';
-import awsconfig from './aws-exports';
+// import { Amplify } from 'aws-amplify';
+// import awsconfig from './aws-exports';
 
-Amplify.configure(awsconfig);
+// Amplify.configure(awsconfig);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
