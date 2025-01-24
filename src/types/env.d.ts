5f2067ba346ca0f41fb05253843df5efc58b8627
Daria Astapenko <50418449+TinaMorskaya@users.noreply.github.com>
2025-01-24 20:19:07 +0100

    installed sentry (#2)

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -2,6 +2,10 @@
 
 interface ImportMetaEnv {
     readonly VITE_BITCOIN_COST_WS_URL: string
+    readonly VITE_SENTRY_DSN: string
+    readonly VITE_SENTRY_AUTH_TOKEN: string
+    readonly VITE_SENTRY_ORG: string
+    readonly VITE_SENTRY_PROJECT: string
 }
 
 interface ImportMeta {
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,26 @@
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [ react() ],
-    test: {
-        globals: true,
-        environment: 'jsdom',
-        css: false,
-        setupFiles: [ './vitest.setup.ts' ],
+export default defineConfig(({mode}) => {
+    const env = loadEnv(mode, process.cwd(), 'VITE_SENTRY_')
+    return {
+        plugins: [ react(), sentryVitePlugin({
+            authToken: env.VITE_SENTRY_AUTH_TOKEN,
+            org: env.VITE_SENTRY_ORG,
+            project: env.VITE_SENTRY_PROJECT,
+        }) ],
+        test: {
+            globals: true,
+            environment: 'jsdom',
+            css: false,
+            setupFiles: [ './vitest.setup.ts' ],
+        },
+        build: {
+            sourcemap: true
+        }
     }
 })
